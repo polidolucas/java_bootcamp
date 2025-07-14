17ba67755eb17a7fb8ac785cefeb17f27cefd7ab
polidolucas <polido.lucas@gmail.com>
2025-07-14 11:39:01 -0300

    IfElse Challenge

---
+++ Geral/src/IfElseChallenge.java
@@ -0,0 +1,17 @@
+public class IfElseChallenge {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score < 5000 && score > 1000){
+            System.out.println("your score was less than 5000 but greater than 1000");
+        } else if (score < 1000){
+            System.out.println("your score was less than 1000");
+        } else {
+            System.out.println("got here");
+        }
+    }
+}
