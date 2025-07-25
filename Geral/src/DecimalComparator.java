6e60e6b979f5258735d71ca8605e8dd732255379
polidolucas <polido.lucas@outlook.com>
2025-07-25 18:28:33 -0300

    decimal comparator

---
+++ Geral/src/DecimalComparator.java
@@ -0,0 +1,10 @@
+public class DecimalComparator {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean areEqualByThreeDecimalsPlaces (double num1, double num2){
+
+        
+    }
+}
