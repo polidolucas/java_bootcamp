57b98293b12fd63d7a3cf3fd5a6c4c5bf544e2ff
polidolucas <polido.lucas@gmail.com>
2025-08-02 10:43:56 -0300

    exercicios

--- Geral/src/DecimalComparator.java
+++ Geral/src/DecimalComparator.java
@@ -1,10 +1,19 @@
 public class DecimalComparator {
     public static void main(String[] args) {
 
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
+
     }
 
-    public static boolean areEqualByThreeDecimalsPlaces (double num1, double num2){
+    public static boolean areEqualByThreeDecimalPlaces (double num1, double num2){
 
-        
+        long n1 = (long) (num1 * 1000);
+        long n2 = (long) (num2 * 1000);
+
+        return n1 == n2;
+
     }
 }
